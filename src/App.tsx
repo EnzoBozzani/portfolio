4cfc58be44bb255be39adde076ff682cbc25e437
Enzo Bozzani <bozzanienzo@gmail.com>
2023-06-07 13:36:35 -0300

    feat: developing tech cards and carousel

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import ArrowBox from "./components/Arrow/ArrowBox";
 import Title from "./components/Title";
 import Accordion from "./components/Accordion";
 import NavBar from "./components/NavBar";
+import Carousel from "./components/Carousel";
+import TechSection from "./components/TechSection";
 
 function App() {
 
@@ -10,8 +12,10 @@ function App() {
     <>
       <NavBar/>
       <Title/>
-      <TextBox text="Hey! 👋 I'm a 19-year-old tech enthusiast, totally into software development. I live in Santos, SP (Brazil), and I'm studying Computer Science at Centro Universitário FEI (university) while diving into Web Development at OneBitCode (online course). As I mentioned, my main focus right now is Web Development because I'm super stoked about working in this field. If you've got any suggestions, feel free to hit me up! I appreciate any help. Thanks!"/>
+      <TextBox text="I'm a 19-year-old tech enthusiast, totally into software development. I live in Santos, SP (Brazil), and I'm studying Computer Science at Centro Universitário FEI (university) while diving into Web Development at OneBitCode (online course). As I mentioned, my main focus right now is Web Development because I'm super stoked about working in this field. If you've got any suggestions, feel free to hit me up! I appreciate any help. Thanks!"/>
       <Accordion/>
+      <TechSection/>
+      <Carousel/>
     </>
   )
 }
---
+++ src/components/TechSection/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./TechSection.module.scss";
+
+export default function TechSection(){
+    return (
+        <div className={styles.wrapper}>
+
+        </div>
+    )
+}
+
+//usar bootstrap cards para as tecnologias (1 card p cada tech, criar um compoonente card aqui nessa pasta mesmo)
