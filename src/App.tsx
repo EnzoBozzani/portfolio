22e5a3d8b1225f50cd29de264aee16c12dc73088
Enzo Bozzani <bozzanienzo@gmail.com>
2023-06-08 02:09:14 -0300

    feat: continued developing cards and made some changes in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import TextBox from "./components/TextBox";
-import ArrowBox from "./components/Arrow/ArrowBox";
 import Title from "./components/Title";
 import Accordion from "./components/Accordion";
 import NavBar from "./components/NavBar";
@@ -16,7 +15,10 @@ function App() {
       <TextBox text="I'm a 19-year-old tech enthusiast, totally into software development. I live in Santos, SP (Brazil), and I'm studying Computer Science at Centro Universitário FEI (university) while diving into Web Development at OneBitCode (online course). As I mentioned, my main focus right now is Web Development because I'm super stoked about working in this field. If you've got any suggestions, feel free to hit me up! I appreciate any help. Thanks!"/>
       <Accordion/>
       <TechSection>
+        <Card img="/html-css.png" title="HTML & CSS" text="I've learned TS through OneBitCode course and it became my favourite language. I use TS in almost every project." url="#"/> 
+        <Card img="/js-logo.png" title="JavaScript" text="My first contact with JS was even before college, when I did a simple JS course in YouTube, given by Curso em Video. Then, I had contact again with JS in college, and that made me very interested in learning Web Development in General. Because of that interest, I started FullStack JavaScript, a course given by OneBitCode. Through this course, I delved deeper into JavaScript." url="#"/> 
         <Card img="/ts-logo.png" title="TypeScript" text="I've learned TS through OneBitCode course and it became my favourite language. I use TS in almost every project." url="#"/> 
+        <Card img="/html-css.png" title="HTML & CSS" text="I've learned TS through OneBitCode course and it became my favourite language. I use TS in almost every project." url="#"/> 
       </TechSection>
       <Carousel/>
     </>
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -3,7 +3,7 @@ import styles from "./NavBar.module.scss";
 export default function NavBar() {
     return (
         <nav className={styles.wrapper}>
-            <a href="">Contact</a><a href="">About</a>
+            <a href="">Projects</a><a href="">Skills</a>
             <a href="https://github.com/EnzoBozzani" target="_blank"><img src="/github.svg" alt="GitHub Logo" className={styles.icon}/></a>
             <a href="https://www.linkedin.com/in/enzo-bozzani-812a7322a/" target="_blank"><img src="/icons8-linkedin.svg" alt="Linkedin Logo" className={styles.icon} /></a>
         </nav>
