7fa95e2c2dfbc22b9e52c854739e3c4bf936fb00
Enzo Bozzani <bozzanienzo@gmail.com>
2023-06-10 01:44:52 -0300

    feat: adding cards content and adding animation to some elements

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,12 @@ function App() {
       <Title/>
       <TextBox text="I'm a 19-year-old tech enthusiast, totally into software development. I live in Santos, SP (Brazil), and I'm studying Computer Science at Centro Universitário FEI (university) while diving into Web Development at OneBitCode (online course). As I mentioned, my main focus right now is Web Development because I'm super stoked about working in this field. If you've got any suggestions, feel free to hit me up! I appreciate any help. Thanks!"/>
       <Accordion/>
+      <p className="display-5 text-center w-100 mb-5" style={{color: "#01003f"}}>Technologies</p>
       <TechSection>
         <Card img="/js-logo.png" title="JavaScript" text="My first contact with JS was even before college, when I did a simple JS course in YouTube, given by Curso em Video. Then, I had contact again with JS in college, and that made me very interested in learning Web Development in General. Because of that interest, I started FullStack JavaScript, a course given by OneBitCode. Through this course, I delved deeper into JavaScript." url="#"/> 
         <Card img="/html-css.png" title="HTML & CSS" text="Just like JavaScript, I had my first contact with HTML and CSS before college, in the same YouTube course. After that, in college, I dealt with these technologies again in the Web Development subject. These technologies, just like JS, were explored in greater depth in the FullStack JavaScript course." url="#"/> 
         <Card img="/ts-logo.png" title="TypeScript" text="I've learned TS through the same course and, since I've discovered it, it became without doubts my most used programming language. Nowadays, I use TypeScript in almost all web projects." url="#"/> 
-        <Card img="/sass-logo.png" title="SCSS/SASS & BootStrap" text="I've learned TS through OneBitCode course and it became my favourite language. I use TS in almost every project." url="#"/> 
+        <Card img="/sass-logo.png" title="SCSS/SASS & BootStrap" text="I've learned TS through OneBitCode course and it became my favourite language. I use TS in almost every project." url="https://github.com/EnzoBozzani/spacex"/> 
         <Card img="/reacr-logo.png" title="React" text="React ..." url="#"/>
       </TechSection>
       <Carousel/>
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,18 +1,23 @@
 import styles from "./Card.module.scss";
 
 const Card = (props: {img: string, title: string, text: string, url: string}) => {
+    function handleClick(){
+        window.open(props.url);
+    }
     return (
-        <div className="card">
+        <div className={`${styles.mainCard} card`}>
             <div className="card-img-top">
-                <img src={props.img}  alt="..."/>
+                <img  data-aos="flip-up" data-aos-duration="1000"  src={props.img}  alt="..."/>
             </div>
                 <div className="card-body">
-                    <h5 className="card-title">{props.title}</h5>
-                    <p className="card-text">{props.text}</p>
-                    <a href={props.url} className={styles.button}>Project using {props.title}</a>
+                    <h5  data-aos="flip-up" data-aos-duration="1000" className="card-title">{props.title}</h5>
+                    <p  data-aos="flip-up" data-aos-duration="1000" className="card-text">{props.text}</p>
+                    <button onClick={handleClick} className={styles.button}>Project using {props.title}</button>
                 </div>
         </div>
     )
 }
 
 export default Card;
+
+//estilizar o botão e adicionar o funcionamento da função
